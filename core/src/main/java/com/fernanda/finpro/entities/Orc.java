f13b584a22712cfa045bd402db2b5f4b8b91697b
apenchuu <steven41@ui.ac.id>
2025-12-21 22:14:02 +0700

    DONT TOUCH

--- core/src/main/java/com/fernanda/finpro/entities/Orc.java
+++ core/src/main/java/com/fernanda/finpro/entities/Orc.java
@@ -1,6 +1,5 @@
 package com.fernanda.finpro.entities;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
